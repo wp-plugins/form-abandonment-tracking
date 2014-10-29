d198eed2080c805ff40b776ff58ce745bcddb98f
downtownrob <downtownrob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-29 00:11:49 +0000

    v1.1 supports Universal Analytics and fixes a bug in tracking submits.
    git-svn-id: https://plugins.svn.wordpress.org/form-abandonment-tracking/trunk@1015566 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form-abandonment-tracking.php
+++ form-abandonment-tracking.php
@@ -3,7 +3,7 @@
  * Plugin Name: Form Abandonment Tracking
  * Plugin URI: http://webwizards.net/wordpress/
  * Description: Tracks form abandonment to the form field level as Google Analytics events, including Submits.
- * Version: 1.0
+ * Version: 1.1
  * Author: Web Wizards
  * Author URI: http://webwizards.net/wordpress/
  * License: GPL2
@@ -15,23 +15,43 @@ function form_tracker() {
 <script type="text/javascript">
 (function($) {
  $(document).ready(function() {
-  $('form').find(':input').blur(function() {
-    if (this.value === this.defaultValue) {
-	var inputAction = 'skipped';
-    } else {
-	var inputAction = this.value ? 'completed' : 'skipped';
-    }
+    $('form').find(':input').blur(function() {
+      if (this.value === this.defaultValue) {
+        var inputAction = 'skipped';
+      } else {
+        var inputAction = this.value ? 'completed' : 'skipped';
+      }
+//      console.log('Form Abandonment Tracking - Detected: ' + (this.form.id || 'form-without-id') + ' Label: ' + this.name + ' Action: ' + inputAction);
+                if (typeof ga !== 'undefined') {
+                    ga('send', 'event', (this.form.id || 'form-without-id'), inputAction, $(this).attr('name'));
+                    console.log('Form Abandonment Tracking - GA Universal in use, sent: ' + (this.form.id || 'form-without-id') + ' Label: ' + this.name + ' Action: ' + inputAction);
+                 }
+                //check if _gaq is set too
+                if (typeof _gaq !== 'undefined') {
+                    _gaq.push(['_trackEvent', (this.form.id || 'form-without-id'), inputAction, $(this).attr('name')]);
+                    console.log('Form Abandonment Tracking - Google Analytics in use, sent: ' + (this.form.id || 'form-without-id') + ' Label: ' + this.name + ' Action: ' + inputAction);
+                }
+  });
+  $('form').find(':input').click(function() {
     if(this.type && this.type.toLowerCase() === 'submit') {
       inputAction = 'submitted';
+//          console.log('Form Abandonment Tracking - Submit clicked: ' + (this.form.id || 'form-without-id') + ' Label: ' + this.name + ' Action: ' + inputAction);
+                if (typeof ga !== 'undefined') {
+                    ga('send', 'event', (this.form.id || 'form-without-id'), inputAction, $(this).attr('name'));
+                    console.log('Form Abandonment Tracking - GA Universal in use, sent: ' + (this.form.id || 'form-without-id') + ' Label: ' + this.name + ' Action: ' + inputAction);
+                 }
+                //check if _gaq is set too
+                if (typeof _gaq !== 'undefined') {
+                    _gaq.push(['_trackEvent', (this.form.id || 'form-without-id'), inputAction, $(this).attr('name')]);
+                    console.log('Form Abandonment Tracking - Google Analytics in use, sent: ' + (this.form.id || 'form-without-id') + ' Label: ' + this.name + ' Action: ' + inputAction);
+                }
     }
-    //alert('tracking form: ' + (this.form.id || 'form-without-id') + ' Label: ' + this.name + ' Action: ' + inputAction);
-    _gaq.push(['_trackEvent', (this.form.id || 'form-without-id'), inputAction, $(this).attr('name')]);
-  });
+ });
  });
 })(jQuery);
 </script>
 <!-- End Form Abandonment Tracker -->
 
 <?php }
-add_action('wp_footer', 'form_tracker', 21);
+add_action('wp_footer', 'form_tracker', 200);
 ?>
